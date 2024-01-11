8f6cd0bf6542906c0cde433fd3b0edcae219e103
Carlos Cabrera <carlos.cabrecar@gmail.com>
2024-01-10 23:16:18 -0600

    "create account" button

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 from kivymd.app import MDApp
 from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.core.window import Window
+
+Window.size = (350, 625)
 
 
 class LoginWindow(Screen):
-    pass
+    main_orange = "FF9843"
 
 
 class MainWindow(Screen):
     pass
 
 
-class AmazonClone(MDApp):
+class AmazonFlex(MDApp):
     def build(self):
         sm = ScreenManager()
-        sm.add_widget(MainWindow(name='main'))
         sm.add_widget(LoginWindow(name='login'))
+        sm.add_widget(MainWindow(name='main'))
 
         return sm
+
+
+if __name__ == "__main__":
+    AmazonFlex().run()
