30e4922a4b98e9e55363122003224b9b37cd8372
Carlos Cabrera <carlos.cabrecar@gmail.com>
2024-01-11 23:45:32 -0600

    credentials screen bases

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from kivymd.app import MDApp
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.core.window import Window
+from kivy.uix.screenmanager import NoTransition
 
 Window.size = (350, 625)
 
@@ -9,13 +10,18 @@ class LoginWindow(Screen):
     main_orange = "FF9843"
 
 
+class Credentials(Screen):
+    pass
+
+
 class MainWindow(Screen):
     pass
 
 
 class AmazonFlex(MDApp):
     def build(self):
-        sm = ScreenManager()
+        sm = ScreenManager(transition=NoTransition())
+        sm.add_widget(Credentials(name='credentials'))
         sm.add_widget(LoginWindow(name='login'))
         sm.add_widget(MainWindow(name='main'))
 
