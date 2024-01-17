ead6b40f2350186e06e723fdce5779784eb083dd
Carlos Cabrera <carlos.cabrecar@gmail.com>
2024-01-17 01:15:36 -0600

    Fixed textinput color

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from kivy.core.window import Window
 from kivy.uix.screenmanager import NoTransition
 
 Window.size = (350, 625)
-
-
+#•••••••
+#TODO fix text input fields not able to same color as line (always light blue)
 class LoginWindow(Screen):
     main_orange = "FF9843"
 
@@ -24,6 +24,7 @@ class AmazonFlex(MDApp):
         sm.add_widget(Credentials(name='credentials'))
         sm.add_widget(LoginWindow(name='login'))
         sm.add_widget(MainWindow(name='main'))
+        self.theme_cls.primary_palette = "Orange"
 
         return sm
 
